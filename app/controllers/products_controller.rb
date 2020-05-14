24366d2f97e24972c89bd230ab9dc04fa928bf9f
louis baptiste <louisvis@ymail.com>
2020-05-14 04:09:32 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,7 +26,7 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     respond_to do |format|
       format.html {render :show}
-      format.json {render json: @product.to_json(only: [:name, :description, :id])}
+      format.json {render json: @product.to_json(only: [:name, :inventory, :id])}
     end
   end
 
